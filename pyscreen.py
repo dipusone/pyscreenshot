e6aa687281d4da3f8feb7253836b29b904f9e9b0
dipus <jacopo.ferrigno@gmail.com>
2019-05-26 19:52:28 +0200

    Simple concept working

--- pyscreen.py
+++ pyscreen.py
@@ -4,7 +4,12 @@ import os
 import sys
 
 from appdirs import user_config_dir
-from PyQt5.QtWidgets import QApplication, QWidget
+from os.path import expanduser
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QKeySequence
+from subprocess import check_call, CalledProcessError
+
 
 
 class Configuration(object):
@@ -61,13 +66,98 @@ class Configuration(object):
         return [Configuration.DEFAULT_CATEGORY]
 
 
+class PyScreen(QWidget):
+
+    def __init__(self):
+        super(PyScreen, self).__init__()
+        self.layout = QGridLayout()
+        self.current_row = 0
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(400, 400, 400, 220)
+        self.setWindowTitle('PyScreen')
+        self.setLayout(self.layout)
+        self._add_select_dir()
+        self._add_define_name()
+        self._add_simple_options()
+        self._add_take_screen()
+        self._init_shortcuts()
+
+    def pick_dir(self):
+        dialog = QFileDialog()
+        folder_path = dialog.getExistingDirectory(None, "Select Folder")
+        self.folder_path.setText(folder_path)
+
+    def _add_select_dir(self):
+        self.folder_path = QLineEdit('Destination Directory', self)
+        # self.label.adjustSize()
+        self.dirBtn = QPushButton('Dir', self)
+        self.dirBtn.clicked.connect(self.pick_dir)
+        self.dirBtn.adjustSize()
+        self.layout.addWidget(self.folder_path, self.current_row, 0)
+        self.layout.addWidget(self.dirBtn, self.current_row, 1)
+        self.current_row += 1
+
+    def _add_define_name(self):
+        self.screen_name = QLineEdit("File name", self)
+        self.layout.addWidget(self.screen_name, self.current_row, 0)
+        self.current_row += 1
+
+    def _add_simple_options(self):
+        self.exitonscreen = QRadioButton("Exit after screen")
+        self.layout.addWidget(self.exitonscreen, self.current_row, 0)
+        self.current_row += 1
+        pass
+
+    def _add_take_screen(self):
+        self.screenBtn = QPushButton('Screen', self)
+        self.screenBtn.clicked.connect(self.take_screenshot)
+        self.exitBtn = QPushButton('Exit', self)
+        self.exitBtn.clicked.connect(self.close)
+        self.layout.addWidget(self.screenBtn, self.current_row, 0)
+        self.layout.addWidget(self.exitBtn, self.current_row, 1)
+        self.current_row += 1
+
+    def _init_shortcuts(self):
+        shortcut = QShortcut(QKeySequence("Ctrl+Return"), self)
+        shortcut.activated.connect(self.take_screenshot)
+
+    def take_screenshot(self):
+        try:
+            command = 'import -quality 60 {}.png'
+            full_path = self.folder_path.text()
+            name = self.screen_name.text()
+            name = name.replace(' ', '_')
+            full_path = os.path.join(full_path, name)
+            check_call(command.format(full_path), shell=True)
+        except CalledProcessError as e:
+            p = PopUp(self)
+            p.setText(e.message)
+            p.show()
+
+    def _exit(self):
+        self.close()
+
+    def _init_config(self):
+        self.config = Configuration()
+        self.config.load()
+
+
+class PopUp(QDialog):
+    def __init__(self, *args, **kwargs):
+        QDialog.__init__(self, *args, **kwargs)
+        self.setLayout(QVBoxLayout())
+        self.label = QLabel(self)
+        self.layout().addWidget(self.label)
+
+    @pyqtSlot(str)
+    def setText(self, text):
+        self.label.setText(text)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
-
+    window = PyScreen()
+    window.show()
     sys.exit(app.exec_())
